f696326b9a361843fdb4c967101ca9db0bfefa98
Krasimir Dimitrov <133798641+krasi-pd-bg@users.noreply.github.com>
2026-01-25 17:31:02 +0200

    tabBarIcons added

--- src/navigation/BottomTabsNavigator.js
+++ src/navigation/BottomTabsNavigator.js
@@ -1,21 +1,38 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import HomeScreen from "../screens/HomeScreen";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CreateTaskScreen from "../screens/CreateTaskScreen";
 
-
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabsNavigator() {
+
     return (
-        <Tab.Navigator>
-            {/* Define your tab screens here */}
-
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Categories" component={CategoriesScreen} />
-            <Tab.Screen name="CreateTask" component={CreateTaskScreen} />
-
+        <Tab.Navigator
+            screenOptions={{
+                safeAreaInsets: { bottom: 10 },
+                tabBarStyle: { height: 60 }
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Ionicons name="calendar-number-outline" size={size} color={color} />,
+                }}
+            />
+            <Tab.Screen
+                name="Categories"
+                component={CategoriesScreen}
+                options={{ tabBarIcon: ({ color, size }) => <Ionicons name="list-outline" size={size} color={color} />, }}
+            />
+            <Tab.Screen
+                name="CreateTask"
+                component={CreateTaskScreen}
+                options={{ tabBarIcon: ({ color, size }) => <Ionicons name="create-outline" size={size} color={color} />, }}
+            />
         </Tab.Navigator>
     );
 }
